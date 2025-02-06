2c53005eb5455e0b76611f75e1f363745148e4b3
Wanff <rowankwang@gmail.com>
2025-02-06 19:12:42 +0000

    tmux bug

--- config/zshrc.sh
+++ config/zshrc.sh
@@ -9,7 +9,7 @@ export TERM="xterm-256color"
 #share various caches on network drive instead of local 
 export UV_CACHE_DIR="/workspace/.cache"
 export HF_HOME="/workspace/.cache"
-export LC_ALL=en_IN.UTF-8
+#export LC_ALL=en_IN.UTF-8
 export LANG=en_IN.UTF-8
 
 ZSH_DISABLE_COMPFIX=true
